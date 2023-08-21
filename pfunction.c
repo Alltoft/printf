c28ccac16f2aa7fc5bec514b69cd0fd35943bca4
Emran <ouakouriimran@gmail.com>
2023-08-21 14:20:18 +0100

    hmmmm

--- pfunction.c
+++ pfunction.c
@@ -3,9 +3,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+/**
+ * _printf - a func that prints everything
+ * @format: what needs to be printed
+ * Return: always 0
+ */
 int _printf(const char *format, ...)
 {
-	va_list (args);
+	va_list(args);
 
 	va_start(args, format);
 	write(1, format, strlen(format));
